d995f2cb3976eafc029cc9f0d3dd0c647e129c18
gmdmgithub <gmika@interia.pl>
2019-08-27 13:28:13 +0200

    user create

--- myapi/__init__.py
+++ myapi/__init__.py
@@ -1,4 +1,13 @@
 from flaskimport Flask
 
+from .extentions import db, guard
+from .models import User
+
 def create_app():
     app = Flask(__name__)
+
+    db.init_app((app))
+    guard.init_app(app,User)
+
+
+    return app
---
+++ myapi/commands.py
@@ -0,0 +1,31 @@
+import click
+from flask.cli import with_appcontext
+
+from .models import User
+from .extentions import guard
+
+@click.command(name="create_users")
+@with_appcontext()
+def create_users():
+    db.create_all()
+    db.session.add(User(
+        username='Alex',
+        password=guard.hash_password('alex123'),
+    ))
+    db.session.add(User(
+        username='Walter',
+        password=guard.hash_password('walter123'),
+        roles='admin'
+    ))
+    db.session.add(User(
+        username='Adam',
+        password=guard.hash_password('adam123'),
+        roles='operator'
+    ))
+    db.session.add(User(
+        username='John',
+        password=guard.hash_password('john123'),
+        roles='operator,admin'
+    ))
+    db.session.commit()
+
